9af75406e0365ffdd3e46efef208245d58e06d6c
Tony Kung <tonykung1997@hotmail.com>
2020-09-02 23:40:29 -0400

    Create build and publitch it to GitHub Pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { transitions, positions, Provider as AlertProvider } from "react-alert";
