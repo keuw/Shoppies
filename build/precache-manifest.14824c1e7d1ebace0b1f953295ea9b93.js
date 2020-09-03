e3003d5e898b2ce70d2d1b3fca6b4eafaea1cae0
Tony Kung <tonykung1997@hotmail.com>
2020-09-03 02:05:57 -0400

    include build

---
+++ build/precache-manifest.14824c1e7d1ebace0b1f953295ea9b93.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bd23bcf4440066274054e4993befed5",
+    "url": "/Shoppies/index.html"
+  },
+  {
+    "revision": "73d4b2f0490cf251f862",
+    "url": "/Shoppies/static/css/2.4433b899.chunk.css"
+  },
+  {
+    "revision": "bae18e9376efd61b97a2",
+    "url": "/Shoppies/static/css/main.a36937d4.chunk.css"
+  },
+  {
+    "revision": "73d4b2f0490cf251f862",
+    "url": "/Shoppies/static/js/2.e44b2655.chunk.js"
+  },
+  {
+    "revision": "b948d2a7727b83687a00ec53ef5267c0",
+    "url": "/Shoppies/static/js/2.e44b2655.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bae18e9376efd61b97a2",
+    "url": "/Shoppies/static/js/main.873e0d29.chunk.js"
+  },
+  {
+    "revision": "23987a68250f1ec03d48",
+    "url": "/Shoppies/static/js/runtime-main.b05288bd.js"
+  },
+  {
+    "revision": "aaf4a312f081038921963fe4332878bf",
+    "url": "/Shoppies/static/media/goldFrame.aaf4a312.png"
+  }
+]);
